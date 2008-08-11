72108c946a28f3fcdaef501c6071c7acf9a9935f
Ryan McCue <ryan@getlilina.org>
2008-08-11 07:20:28 +0000

    Move admin JS functions into a global "admin" object.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@199 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/js/admin.js
+++ inc/js/admin.js
@@ -1,3 +1,43 @@
+var admin = {
+	init: function () {
+		/** Hide some stuff */
+		$("#changer").hide();
+		$("#changer_id").hide();
+		
+		/** Set up events */
+		$(".change_link").click(function () {
+			feed_url = $(this).parent().siblings(".url-col").text();
+			feed_name = $(this).parent().siblings(".name-col").text();
+			feed_id= $(this).parent().siblings(".id-col").text();
+			$("#change_url").val(feed_url);
+			$("#change_name").val(feed_name);
+			$("#change_id").val(feed_id);
+			$("#changer").slideDown();
+			return false;
+		});
+		$("#alert .tech_notice a.note_link").click(function () {
+			$(this).siblings(".actual_notice").show();
+			$(this).remove();
+		});
+		$("#add_form").submit(function () {
+			feeds.add();
+			return false;
+		});
+		$("#change_form").submit(function () {
+			feeds.change();
+			return false;
+		});
+		
+		/** Add some content */
+		$("#alert .tech_notice").append("<a href=\"javascript:void(0)\" class=\"note_link\">Show technical explanation</a>").children(".actual_notice").hide();
+		
+		/** Make it look pretty */
+		$("#alert").effect("highlight", { 
+			color: "red" 
+		}, 3000);
+	}
+};
+
 var feeds = {
 	add: function () {
 		if( !$("#add_url").val() ) {
@@ -55,41 +95,5 @@ var feeds = {
 };
 
 $(document).ready(function () {
-	$("#navigation li.current").hover(function () {
-		$("#navigation").addClass("hover");
-	}, function () {
-		$("#navigation").removeClass("hover");
-	});
-
-	$("#changer").hide();
-	$("#changer_id").hide();
-	$(".change_link").click(function () {
-		feed_url = $(this).parent().siblings(".url-col").text();
-		feed_name = $(this).parent().siblings(".name-col").text();
-		feed_id= $(this).parent().siblings(".id-col").text();
-		$("#change_url").val(feed_url);
-		$("#change_name").val(feed_name);
-		$("#change_id").val(feed_id);
-		$("#changer").slideDown();
-		return false;
-	});
-	$("#alert .tech_notice").append("<a href=\"javascript:void(0)\" class=\"note_link\">Show technical explanation</a>").children(".actual_notice").hide();
-	$("#alert .tech_notice a.note_link").click(function () {
-		$(this).siblings(".actual_notice").show();
-		$(this).remove();
-	});
-	
-	$("#alert").effect("highlight", { 
-		color: "red" 
-	}, 3000);
-	
-	/* Feeds */
-	$("#add_form").submit(function () {
-		feeds.add();
-		return false;
-	});
-	$("#change_form").submit(function () {
-		feeds.change();
-		return false;
-	});
+	admin.init();
 });
