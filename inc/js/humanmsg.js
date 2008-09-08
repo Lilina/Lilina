d13455a87f6141a12f5dffaa776b26eb0d744fd2
Ryan McCue <ryan@getlilina.org>
2008-09-08 01:08:10 +0000

    Updates to humanmsg.js and admin.css, thanks to Habari for some code.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@215 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/js/humanmsg.js
+++ inc/js/humanmsg.js
@@ -2,9 +2,12 @@
 	HUMANIZED MESSAGES 1.0
 	idea - http://www.humanized.com/weblog/2006/09/11/monolog_boxes_and_transparent_messages
 	home - http://humanmsg.googlecode.com
+	
+	Thanks to Habari for some of this code.
 */
 
 var humanMsg = {
+	msgcount: 0,
 	setup: function(appendTo, logName, msgOpacity) {
 		humanMsg.msgID = 'humanMsg';
 		humanMsg.logID = 'humanMsgLog';
@@ -20,44 +23,47 @@ var humanMsg = {
 		// Opacity of the message
 		humanMsg.msgOpacity = .8;
 
-		if (msgOpacity != undefined) 
+		if (msgOpacity != undefined)
 			humanMsg.msgOpacity = parseFloat(msgOpacity);
 
 		// Inject the message structure
-		jQuery(appendTo).append('<div id="'+humanMsg.msgID+'" class="humanMsg"><div class="round"></div><p></p><div class="round"></div></div> <div id="'+humanMsg.logID+'"><p>'+logName+'</p><ul></ul></div>')
-		
-		jQuery('#'+humanMsg.logID+' p').click( function() {
-			jQuery(this).siblings('ul').slideToggle('50')
-		})
+		jQuery(appendTo).append('<div id="'+humanMsg.msgID+'" class="humanMsg"><div class="imsgs"></div></div><div id="'+humanMsg.logID+'"><p>'+logName+'</p><ul></ul></div>')
+
+		jQuery('#'+humanMsg.logID+' p').click(
+			function() { jQuery('ul', '#'+humanMsg.logID).slideToggle(); jQuery('#humanMsgLog').addClass('logisopen') }	
+		)
+
+		jQuery('#'+humanMsg.logID+' ul').click(
+			function() { jQuery(this).slideToggle(); jQuery('#humanMsgLog').removeClass('logisopen') }
+		)
 	},
 
 	displayMsg: function(msg) {
-		// No message, no need to run.
-		if (msg == '') return;
+		if (msg == '')
+			return;
 
-		// Kill the auto-fadeout timer
 		clearTimeout(humanMsg.t2);
+		humanMsg.msgcount++;
 
 		// Inject message
-		jQuery('#'+humanMsg.msgID+' p').html(msg)
-	
-		// Show message
-		jQuery('#'+humanMsg.msgID+'').show().animate({ opacity: humanMsg.msgOpacity}, 200, function() {
+		$('#'+humanMsg.msgID).show();
+		$('<div class="msg" id="msgid_' + humanMsg.msgcount + '"><p>' + msg + '</p></div>')
+		.appendTo('#'+humanMsg.msgID+' .imsgs')
+		.show().animate({ opacity: humanMsg.msgOpacity}, 200, function() {
 			jQuery('#'+humanMsg.logID)
 				.show().children('ul').prepend('<li>'+msg+'</li>')	// Prepend message to log
 				.children('li:first').slideDown(200)				// Slide it down
-		
-			
+
 			if ( jQuery('#'+humanMsg.logID+' ul').css('display') == 'none') {
-				jQuery('#'+humanMsg.logID+' p').animate({ bottom: 40 }, 200, 'linear', function() {
-					jQuery(this).animate({ bottom: 0 }, 300, 'easeOutBounce', function() { jQuery(this).css({ bottom: 0 }) })
+				jQuery('#'+humanMsg.logID+' p').animate({ bottom: 40 }, 200, function() {
+					jQuery(this).animate({ bottom: 0 }, 300, function() { jQuery(this).css({ bottom: 0 }) })
 				})
 			}
-			
+
 		})
 
 		// Watch for mouse & keyboard in .5s
-		humanMsg.t1 = setTimeout("humanMsg.bindEvents()", 700)
+	//	humanMsg.t1 = setTimeout("humanMsg.bindEvents()", 700)
 		// Remove message after 5s
 		humanMsg.t2 = setTimeout("humanMsg.removeMsg()", 5000)
 	},
@@ -77,9 +83,11 @@ var humanMsg = {
 			.unbind('click', humanMsg.removeMsg)
 			.unbind('keypress', humanMsg.removeMsg)
 
-		// Only if message is still at peak opacity, fade it out
-		if (jQuery('#'+humanMsg.msgID).css('opacity') == humanMsg.msgOpacity)
-			jQuery('#'+humanMsg.msgID).animate({ opacity: 0 }, 500, function() { jQuery(this).hide() })
+		// If message is fully transparent, fade it out
+		jQuery('#'+humanMsg.msgID+ ' .imsgs .msg').each(function(){
+			if (jQuery(this).css('opacity') == humanMsg.msgOpacity)
+				jQuery(this).animate({ opacity: 0 }, 500, function() { jQuery(this).remove() })
+		});
 	}
 };
 
