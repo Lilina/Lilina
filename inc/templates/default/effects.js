78ba8915882442ff918bd6bd1360f82bd53316a8
Ryan McCue <ryan@getlilina.org>
2008-12-09 04:36:16 +0000

    Fix JS if loading through JS (default template)
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@281 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/templates/default/effects.js
+++ inc/templates/default/effects.js
@@ -1,18 +1,7 @@
 /* JQuery Stuff! */
 $(document).ready(function() {
-	$('.river-page .excerpt').hide().parent().addClass('c1').removeClass('c2');
-	$('.river-page h1')
-		.css( {cursor:'pointer'} )
-		.click(function() {
-			$(this).next().slideToggle();
-	});
-	$('.river-page .title').click(function() {
-		
-		$(this).siblings('.excerpt').slideToggle();
-		$(this).parent().toggleClass('c1').toggleClass('c2');
-		
-		return false;
-	});
+
+	/* Setup our global buttons */
 	$('.river-page #expandall').css({cursor:'pointer'}).click(function() {
 		
 		$('.excerpt').slideDown();
@@ -27,16 +16,30 @@ $(document).ready(function() {
 		
 		return false;
 	});
+	
+	/* Ajax loading if no items are found */
 	$('.river-page #viewallitems').click(function() {
-		$('#main').load('index.php?hours=-1 #main').children().toggle();
-		
-		$('.title').siblings('.excerpt').slideUp().parent().addClass('c1').removeClass('c2');
-		
-		$('.river-page .title').click(function() {
-			$(this).siblings().slideToggle().parent().toggleClass('c1').toggleClass('c2');
-			return false;
+		$('#main').load('index.php?hours=-1 #main', {}, function () {
+			setup_items();
 		});
 		
 		return false;
 	});
+	
+	/* Per-item setup */
+	setup_items();
 });
+
+function setup_items() {
+	$('.river-page .excerpt').hide().parent().addClass('c1').removeClass('c2');
+	$('.river-page h1')
+		.css( {cursor:'pointer'} )
+		.click(function() {
+			$(this).next().slideToggle();
+	});
+	$('.river-page .title').click(function() {
+		$(this).siblings('.excerpt').slideToggle();
+		$(this).parent().toggleClass('c1').toggleClass('c2');
+		return false;
+	});
+}
