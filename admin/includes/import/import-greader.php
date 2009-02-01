f39f595d6f5227d106975940dd41939e403054fb
Ryan McCue <ryan@getlilina.org>
2009-02-01 12:01:05 +0000

    New importers. Guaranteed to be broken, as they are unlinked.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@316 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- admin/includes/import/import-greader.php
+++ admin/includes/import/import-greader.php
@@ -1,3 +1,24 @@
+<?php
+
+require_once(LILINA_PATH . '/admin/includes/import/import-other.php');
+
+class GoogleReader_Import extends OPML_Import {
+	public function dispatch() {
+		$step = ( !empty($_POST['step']) ? $_POST['step'] : 0 );
+		switch($step) {
+			case 0:
+				$this->introduction();
+				break;
+			case 1:
+				$this->import();
+				break;
+		}
+	}
+
+	public function introduction() {
+	}
+	public function form() {
+?>
 <form action="feed-import.php" method="get" id="import_form">
 	<fieldset id="import">
 		<legend><?php _e('Import Feeds'); ?></legend>
@@ -8,3 +29,9 @@
 		<input type="submit" value="<?php _e('Import'); ?>" class="submit" name="submit" />
 	</fieldset>
 </form>
+<?php
+	}
+}
+
+$greader_importer = new GoogleReader_Import;
+register_importer('greader', _r('Google Reader'), _r('Import feeds from Google Reader'), array(&$greader_importer, 'dispatch'));
--- admin/includes/import/import-other.php
+++ admin/includes/import/import-other.php
@@ -1,47 +1,60 @@
 <?php
 
-/**
- * Parse feeds into an array, ready to pass to the Javascript importer
- *
- * @param string $opml_url URL to parse feed data from, in the OPML standard.
- * @return array Associative array containing feed URL, title and category (if applicable)
- */
-function import_opml($opml_url) {
-	if(empty($opml_url)) {
-		MessageHandler::add_error(sprintf(_r('No OPML specified')));
-		return false;
+class OPML_Import {
+	public function dispatch() {
+		$step = ( !empty($_POST['step']) ? $_POST['step'] : 0 );
+		switch($step) {
+			case 0:
+				$this->introduction();
+				break;
+			case 1:
+				$this->import();
+				break;
+		}
 	}
-
-	require_once(LILINA_INCPATH . '/contrib/simplepie/simplepie.inc');
-	$opml = new SimplePie_File($opml_url);
-	$opml = new OPML($opml->body);
-
-	if(!empty($opml->error) || empty($opml->data)) {
-		MessageHandler::add_error(sprintf(_r('The OPML file could not be read. The parser said: %s'), $opml->error));
-		return false;
-	}
-	$feeds_num = 0;
-	foreach($opml->data as $cat => $feed) {
-		if(!isset($feed['xmlurl']) && isset($feed[0]['xmlurl'])) {
-			foreach($feed as $subfeed) {
-				$feeds[] = array('url' => $subfeed['xmlurl'], 'title' => $subfeed['title'], 'cat' => $cat);
-				++$feeds_num;
-			}
-			continue;
+	/**
+	 * Parse feeds into an array, ready to pass to the Javascript importer
+	 *
+	 * @param string $opml_url URL to parse feed data from, in the OPML standard.
+	 * @return array Associative array containing feed URL, title and category (if applicable)
+	 */
+	function import_opml($opml_url) {
+		if(empty($opml_url)) {
+			MessageHandler::add_error(sprintf(_r('No OPML specified')));
+			return false;
 		}
 
-		$feeds[] = array('url' => $feed['xmlurl'], 'title' => $feed['title'], 'cat' => '');
-		++$feeds_num;
-	}
-	MessageHandler::add(sprintf(__ngettext('Adding %d feed', 'Adding %d feeds', $feeds_num), $feeds_num));
-	return $feeds;
-}
+		require_once(LILINA_INCPATH . '/contrib/simplepie/simplepie.inc');
+		$opml = new SimplePie_File($opml_url);
+		$opml = new OPML($opml->body);
 
+		if(!empty($opml->error) || empty($opml->data)) {
+			MessageHandler::add_error(sprintf(_r('The OPML file could not be read. The parser said: %s'), $opml->error));
+			return false;
+		}
+		$feeds_num = 0;
+		foreach($opml->data as $cat => $feed) {
+			if(!isset($feed['xmlurl']) && isset($feed[0]['xmlurl'])) {
+				foreach($feed as $subfeed) {
+					$feeds[] = array('url' => $subfeed['xmlurl'], 'title' => $subfeed['title'], 'cat' => $cat);
+					++$feeds_num;
+				}
+				continue;
+			}
+
+			$feeds[] = array('url' => $feed['xmlurl'], 'title' => $feed['title'], 'cat' => '');
+			++$feeds_num;
+		}
+		MessageHandler::add(sprintf(__ngettext('Adding %d feed', 'Adding %d feeds', $feeds_num), $feeds_num));
+		return $feeds;
+	}
+
+/*
 admin_header(_r('Import OPML'), 'feeds.php');
 
 $importing = false;
 
-/** Make sure we're actually adding */
+/** Make sure we're actually adding * /
 if(isset($_REQUEST['submit'])) {
 	if(!isset($_REQUEST['url']))
 		MessageHandler::add_error(_r('No URL specified to import OPML from'));
@@ -49,7 +62,9 @@ if(isset($_REQUEST['submit'])) {
 		$importing = import_opml($_REQUEST['url']);
 }
 
-if(!$importing) {
+	protected function introduction() {
+?>
+<p><?php _e('If a feed reader you use allows you to export your links or subscriptions as OPML you may import them here.'); ?></p>
 ?>
 <form action="feed-import.php" method="get" id="import_form">
 	<fieldset id="import">
@@ -62,9 +77,11 @@ if(!$importing) {
 	</fieldset>
 </form>
 <?php
-}
-else
-	import($importing);
 
-admin_footer();
+	}
+*/
+}
+
+$opml_importer = new OPML_Import;
+register_importer('OPML', _r('Other (OPML)'), _r('Import feeds from an OPML file'), array(&$opml_importer, 'dispatch'));
 ?>
