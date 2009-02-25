199887f4e5260979e6cc9f7204d0563f265b6151
Ryan McCue <ryan@getlilina.org>
2009-02-25 09:26:03 +0000

    Restructure some of the admin Javascript and comment some of it.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@326 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- admin/admin.js
+++ admin/admin.js
@@ -1,99 +1,159 @@
+/**
+ * Contains administrative functions for general pages
+ */
 var admin = {
+	/**
+	 * Initialise the page
+	 */
 	init: function () {
 		/** Hide some stuff */
 		$("#changer, #changer_id").hide();
-		$("form#add_form").append('<p class="loading">Adding feed...</p>');
-		$("form#change_form").append('<p class="loading">Changing feed...</p>');
-		
+		$("fieldset#add").append('<p class="loading">Adding feed...</p>');
+		$("fieldset#change").append('<p class="loading">Changing feed...</p>');
+
 		/** Set up events */
-		$(".change_link").click(admin.setup_change_js);
-		$("#add_form").submit(function () {
-			admin.disable_button(this);
-			
-			feeds.add($("#add_url").val(), $("#add_name").val(), false, feeds.reload_table);
-			
-			admin.enable_button(this);
+		$(".change_link").live('click', function() {
+			$("#change_url").val(
+				$(this).parent().siblings(".url-col").text()
+			);
+			$("#change_name").val(
+				$(this).parent().siblings(".name-col").text()
+			);
+			$("#change_id").val(
+				$(this).parents("tr:first").attr("id").split("-")[1]
+			);
+			$("#changer").slideDown();
 			return false;
 		});
+
+		$("#add_form").submit(function () {
+			feeds.add();
+			return false;
+		});
+
 		$("#change_form").submit(function () {
 			feeds.change();
 			return false;
 		});
-		
+
 		/** Make it look pretty */
 		$("#alert").effect("highlight", { 
 			color: "red" 
 		}, 3000);
-		
+
 		$("#navigation li.has-submenu").hover(function() {
 			$(this).addClass('hovering');
 		}, function() {
 			$(this).removeClass('hovering');
 		});
 	},
-	setup_change_js: function() {
-		$("#change_url").val( $(this).parent().siblings(".url-col").text() );
-		$("#change_name").val( $(this).parent().siblings(".name-col").text() );
-		$("#change_id").val( $(this).parents("tr:first").attr("id").split("-")[1] );
-		$("#changer").slideDown();
-		return false;
-	}
+	/**
+	 * Disables submission buttons within a form passed as the selector parameter
+	 *
+	 * Disables submit buttons then shows a loading indicator
+	 *
+	 * @param selector CSS selector, or jQuery object
+	 */
 	disable_button: function (selector) {
 		$(selector)
 			.children("input.submit")
 				.attr('disabled', 'disabled')
+				.hide()
 			.end()
 			.children(".loading")
 				.show();
 	},
+	/**
+	 * Enables submission buttons within a form passed as the selector parameter
+	 *
+	 * Enables submit buttons then hides the loading indicator
+	 *
+	 * @param selector CSS selector, or jQuery object
+	 */
 	enable_button: function (selector) {
 		$(selector)
 			.children("input.submit")
 				.attr('disabled', '')
+				.show()
 			.end()
 			.children(".loading")
 				.hide();
+	},
+	/**
+	 * Administration Ajax API
+	 */
+	ajax: {
+		/**
+		 * Send a remote request to the admin ajax API
+		 *
+		 * @param {String} action "action" parameter to send to admin-ajax.php
+		 * @param {Array} data Extra data to send to admin-ajax.php
+		 * @param callback Callback function to call on successful request
+		 * @param {String} return_type A valid jQuery dataType as per http://docs.jquery.com/Ajax/jQuery.ajax#options
+		 * @param {String} type A valid HTTP request type. Usually "POST" or "GET"
+		 */
+		request: function (action, data, callback, return_type, type) {
+			if (!action) {
+				return false;
+			}
+			data = data || {};
+			callback = callback || false;
+			return_type = return_type || 'json';
+			type = type || 'GET';
+			
+			data.action = action;
+			data.type = type;
+			$.ajax({
+				type: type,
+				url: "admin-ajax.php",
+				data: data,
+				success: callback,
+				dataType: return_type
+			})
+		},
+		/**
+		 * Convienience function for GET requests
+		 * @see #request
+		 */
+		get: function (action, data, callback, return_type) {
+			admin.ajax.request(action, data, callback, return_type, "GET");
+		},
+		/**
+		 * Convienience function for POST requests
+		 * @see #request
+		 */
+		post: function (action, data, callback, return_type) {
+			admin.ajax.request(action, data, callback, return_type, "POST");
+		},
 	}
 };
 
 var feeds = {
 	count: 0,
 
-	add: function (url, name, errors_only, callback, callback2) {
-		if(errors_only == undefined)
-			errors_only = false;
-
-		if( !url ) {
+	add: function () {
+		if( !$("#add_url").val() ) {
 			humanMsg.displayMsg('No feed URL supplied', 'error');
 			return false;
 		}
-		$.post("admin-ajax.php", {
-			action: "add",
-			type: "json",
-			name: name,
-			url: url
-		}, function (data) {
-			if(!data.errors || data.errors.length == 0) {
-				// Clear the values
-				$("#add_url, #add_name").val('');
+		admin.disable_button("#add_form");
+		admin.ajax.post('add', {name: $("#add_name").val(), url: $("#add_url").val()}, feeds.add_callback);
+	},
+	add_callback: function (data) {
+		admin.enable_button("#add_form");
+		if(!data.errors || data.errors.length == 0) {
+			// Clear the values
+			$("#add_url, #add_name").val('');
 
-				if(!errors_only) {
-					jQuery.each(data.messages, function (index, message) {
-						humanMsg.displayMsg(message['message']);
-					});
-				}
-				if(callback != undefined)
-					callback.call(null, data);
-				return;
-			}
-			jQuery.each(data.errors, function(index, error) {
-				humanMsg.displayMsg(error['message'], 'error', -1);
+			jQuery.each(data.messages, function (index, message) {
+				humanMsg.displayMsg(message['message']);
 			});
-		},
-		"json"
-		);
-		if(callback2 != undefined)
-			callback2.call(null, data);
+			feeds.reload_table();
+			return;
+		}
+		jQuery.each(data.errors, function(index, error) {
+			humanMsg.displayMsg(error['message'], 'error', -1);
+		});
 	},
 	change: function () {
 		if( !$("#change_url").val() ) {
@@ -104,34 +164,25 @@ var feeds = {
 			humanMsg.displayMsg('No feed ID supplied', 'error');
 			return false;
 		}
-		$.post("admin-ajax.php", {
-			action: "change",
-			type: "json",
-			feed_id: $("#change_id").val(),
-			name: $("#change_name").val(),
-			url: $("#change_url").val()
-		}, function (data) {
-			if(!data.errors || data.errors.length == 0) {
-				// Clear the values
-				$("#change_url, #change_name, #change_id").val('');
+		admin.ajax.post('change', {feed_id: $("#change_id").val(), name: $("#change_name").val(), url: $("#change_url").val()}, feeds.add_callback);
+	change_callback: function (data) {
+		if(!data.errors || data.errors.length == 0) {
+			// Clear the values
+			$("#change_url, #change_name, #change_id").val('');
 
-				jQuery.each(data.messages, function (index, message) {
-					humanMsg.displayMsg(message['message']);
-				});
-				feeds.reload_table();
-				return;
-			}
-			jQuery.each(data.errors, function(index, error) {
-				humanMsg.displayMsg(error['message'], 'error');
+			jQuery.each(data.messages, function (index, message) {
+				humanMsg.displayMsg(message['message']);
 			});
-		},
-		"json"
-		);
+			feeds.reload_table();
+			return;
+		}
+		jQuery.each(data.errors, function(index, error) {
+			humanMsg.displayMsg(error['message'], 'error');
+		});
 	},
 	reload_table: function () {
 		$.get("admin-ajax.php", {action: "list", type: "raw"}, function (data) {
 			$("#feeds_list tbody").html(data);
-			$(".change_link").click(admin.setup_change_js);
 		});
 	},
 	begin_processing: function (items) {
