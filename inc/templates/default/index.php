407d5cdadc6d7d0e7c72a5b0183c954ebacf7ddc
Ryan McCue <ryan@getlilina.org>
2009-04-17 03:01:06 +0000

    Remove reference to jquery.hotkeys.js as we don't use it. Props Khalsa.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@351 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/templates/default/index.php
+++ inc/templates/default/index.php
@@ -18,7 +18,6 @@ header('Content-Type: text/html; charset=utf-8');
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 <script language="JavaScript" type="text/javascript" src="<?php echo get_option('baseurl') ?>inc/js/jquery.js"></script>
 <script language="JavaScript" type="text/javascript" src="<?php echo get_option('baseurl') ?>inc/js/jquery.scrollTo.js"></script>
-<script language="JavaScript" type="text/javascript" src="<?php template_directory(); ?>/jquery.hotkeys.js"></script>
 <script language="JavaScript" type="text/javascript" src="<?php template_directory(); ?>/effects.js"></script>
 <?php
 template_header();
