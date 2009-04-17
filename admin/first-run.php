bdc30035e14140e9c914437abe61825ad116cf53
Ryan McCue <ryan@getlilina.org>
2009-04-17 07:21:35 +0000

    Add the first-run page.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@364 13ecc93d-8f1e-0410-9266-85e8f28b97b7

---
+++ admin/first-run.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * First run tools, such as the importer, on a single page
+ *
+ * @author Ryan McCue <cubegames@gmail.com>
+ * @package Lilina
+ * @subpackage Administration
+ * @version 1.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+/** */
+require_once('admin.php');
+
+admin_header(_r('First-Run'));
+?>
+<h1><?php _e('Welcome') ?></h1>
+<p><?php _e("To help you settle in, we've included a few nifty tools in Lilina, just to help you get started.") ?></p>
+<h2><?php _e('Import') ?></h2>
+<p><?php _e("We can import from any service which supports an open standard called OPML. Here's some services you can import from:") ?></p>
+<ul li="block-list">
+	<li><a href="feed-import.php?service=greader"><?php _e('Google Reader') ?></a></li>
+	<li><a href="feed-import.php?service=bloglines"><?php _e('Bloglines') ?></a></li>
+	<li><a href="feed-import.php?service=other"><?php _e('Others') ?></a></li>
+</ul>
+<p class="sidenote"><?php echo sprintf(_r('Looking to import from another service? Try our <a href="%s">open documentation</a> to see what other users have found.'), 'http://getlilina.org/wiki/importing') ?></p>
+<h2><?php _e('Quick Adding') ?></h2>
+<p><?php _e('Use this bookmarlet to subscribe to feeds straight from your browser:')?>
+<a href="javascript:void(sp=window.open(<?phpecho get_option('baseurl')?>/admin/subscribe.php?url='+escape(document.location),'lilina','toolbar=no,width=800,height=600,scrollbars=yes'));%20void(setTimeout(function(){sp.focus()},100));"><?php _e('Subscribe') ?></a></p>
+<?php
+admin_footer();
