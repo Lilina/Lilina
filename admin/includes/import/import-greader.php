3dbeddce40f929865119c0be1b5ef0da14935f7a
Ryan McCue <ryan@getlilina.org>
2009-06-26 05:16:40 +0000

    Add documentation for the Google Reader import page, until we get proper importing.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@421 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- admin/includes/import/import-greader.php
+++ admin/includes/import/import-greader.php
@@ -17,8 +17,39 @@ require_once(LILINA_PATH . '/admin/includes/import/import-other.php');
  * @package Lilina
 */
 class GoogleReader_Import extends OPML_Import {
-	/*public function introduction() {
-	}*/
+	public function introduction() {
+		admin_header(_r('Google Reader Importer'));
+?>
+<h1><?php _e('Google Reader Importer') ?></h1>
+<p><?php _e('There are several ways to import from Google Reader.'); ?></p>
+<h2><?php _e('Method 1'); ?></h2>
+<p><?php printf(
+	_r('<a href="%1$s">Export</a> your feeds from Google reader and then use the <a href="%2$s">OPML importer</a>.'),
+	'http://www.google.com/reader/subscriptions/export',
+	'feed-import.php?service=opml'
+	); ?></p>
+<h2><?php _e('Method 2'); ?></h2>
+<p><?php _e("We can grab your OPML file for you, but we'll need your username and password. This information won't be stored anywhere and is only used once. (It sucks, we know, but Google doesn't offer any other way.)"); ?>
+<form action="feed-import.php" method="POST">
+	<button id="continue"><?php _e('Start Method 2'); ?></button>
+	<fieldset id="greader">
+		<legend><?php _e('Import Feeds'); ?></legend>
+		<div class="row">
+			<label for="user"><?php _e('Username (Email address)'); ?>:</label>
+			<input type="text" name="user" id="user" />
+		</div>
+		<div class="row">
+			<label for="pass"><?php _e('Password'); ?>:</label>
+			<input type="password" name="pass" id="pass" />
+		</div>
+		<input type="submit" value="<?php _e('Import'); ?>" class="submit" name="submit" />
+		<input type="hidden" name="step" value="1" />
+		<input type="hidden" name="service" value="opml" />
+	</fieldset>
+</form>
+<?php
+		admin_footer();
+	}
 }
 
 $greader_importer = new GoogleReader_Import;
