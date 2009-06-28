3562cda2926b68f9e4a664f9e6738c4212ac8977
Ryan McCue <ryan@getlilina.org>
2009-06-28 09:32:32 +0000

    Define the user agent in a single spot.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@432 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/core/version.php
+++ inc/core/version.php
@@ -24,4 +24,6 @@ $lilina = array(
 		'version'	=> LILINA_SETTINGS_VERSION,
 	),
 );
+
+define('LILINA_USERAGENT', 'Lilina/'. LILINA_CORE_VERSION . '; (' . get_option('baseurl') . '; http://getlilina.org/; Allow Like Gecko)');
 ?>
