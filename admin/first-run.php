c4d0e7df05a3b77eae93d6d80233d0b7f9e0e6e2
Ryan McCue <ryan@getlilina.org>
2009-09-11 14:32:31 +0000

    Make the first run page warmer and fuzzier.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@466 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- admin/first-run.php
+++ admin/first-run.php
@@ -18,10 +18,11 @@ admin_header(_r('First-Run'));
 <p><?php _e("To help you settle in, we've included a few nifty tools in Lilina, just to help you get started.") ?></p>
 <h2><?php _e('Import') ?></h2>
 <p><?php _e("We can import from any service which supports an open standard called OPML. Here's some services you can import from:") ?></p>
-<ul li="block-list">
-	<li><a href="feed-import.php?service=greader"><?php _e('Google Reader') ?></a></li>
-	<li><a href="feed-import.php?service=bloglines"><?php _e('Bloglines') ?></a></li>
-	<li><a href="feed-import.php?service=other"><?php _e('Others') ?></a></li>
+<ul id="block-list">
+	<li class="greader"><a href="feed-import.php?service=greader"><?php _e('Google Reader') ?></a></li>
+	<li class="bloglines"><a href="feed-import.php?service=bloglines"><?php _e('Bloglines') ?></a></li>
+	<li class="other"><a href="feed-import.php?service=opml"><?php _e('OPML (desktop readers)') ?></a></li>
+	<li class="other"><a href="feed-import.php?service=other"><?php _e('Others') ?></a></li>
 </ul>
 <p class="sidenote"><?php echo sprintf(_r('Looking to import from another service? Try our <a href="%s">open documentation</a> to see what other users have found.'), 'http://getlilina.org/wiki/importing') ?></p>
 <h2><?php _e('Quick Adding') ?></h2>
