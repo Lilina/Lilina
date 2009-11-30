52224316b6a0234da1a1154d789b2fe812acbbaa
Ryan McCue <ryan@getlilina.org>
2009-11-30 08:24:47 +0000

    Clean up admin JS and CSS a bit.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@516 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- admin/importer.js
+++ admin/importer.js
@@ -9,19 +9,20 @@ var importer = {
 	},
 	iterate: function () {
 		var feed = importer.feeds.shift();
-		if(feed == undefined) {
-			if(this.end_callback != null)
+		if(feed === undefined) {
+			if(this.end_callback !== null) {
 				this.end_callback();
+			}
 			return;
 		}
 
 		var me = this;
 		$.ajax({
-			data: {name: feed['title'], url: feed['url'], method: "feeds.add"},
+			data: {name: feed.title, url: feed.url, method: "feeds.add"},
 			dataType: "json",
 			complete: function(request) {
 				try {
-					data = JSON.parse(request.responseText);
+					var data = JSON.parse(request.responseText);
 					me.log(data);
 					me.iterate();
 				} catch(e) {
@@ -31,10 +32,9 @@ var importer = {
 			},
 			type: "POST",
 			url: "admin-ajax.php"
-		})
+		});
 	},
 	log: function (data) {
-		console.log(data);
 		if(data.error) {
 			$("#log").append('<li class="error">' + data.msg + '</li>');
 		}
