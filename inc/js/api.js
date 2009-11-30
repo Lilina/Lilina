6b7aa614340625a95e83b3582bfc1296260cb110
Ryan McCue <ryan@getlilina.org>
2009-11-30 04:33:11 +0000

    Add Javascript API handler.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@511 13ecc93d-8f1e-0410-9266-85e8f28b97b7

---
+++ inc/js/api.js
@@ -0,0 +1,24 @@
+function LilinaAPI(location) {
+	this.location = location;
+}
+LilinaAPI.prototype.location = '';
+LilinaAPI.prototype.call = function (method, params, callback, error_callback, type) {
+	params = params || {};
+	callback = callback || false;
+	error_callback = error_callback || false;
+	var request_params = { 'method': method };
+
+	$.extend(request_params, params);
+
+	$.ajax({
+		'cache': false,
+		'data': request_params,
+		'dataType': 'json',
+		'error': error_callback,
+		'success': callback,
+		'type': (type) ? type : 'GET',
+		'url': this.location
+	});
+};
+LilinaAPI.prototype.get = function (method, params, callback, error_callback) { return this.call(method, params, callback, error_callback, "GET"); };
+LilinaAPI.prototype.post = function (method, params, callback, error_callback) { return this.call(method, params, callback, error_callback, "POST"); };
