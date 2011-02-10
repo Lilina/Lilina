919b2713e71416b0475525007861de381c773915
Ryan McCue <ryan@getlilina.org>
2011-02-10 11:46:07 +0000

    Use `new stdClass` instead of forcing a typecast later.
    git-svn-id: http://lilina.googlecode.com/svn/trunk/lilina@729 13ecc93d-8f1e-0410-9266-85e8f28b97b7

--- inc/core/update-functions.php
+++ inc/core/update-functions.php
@@ -32,7 +32,7 @@ function lilina_version_check() {
 	)
 		return false;
 
-	$new_option = '';
+	$new_option = new stdClass;
 	$new_option->last_checked = time(); // this gets set whether we get a response or not, so if something is down or misconfigured it won't delay the page load for more than 3 seconds, twice a day
 	$new_option->version_checked = $lilina_version;
 
