19240ee0f3d7d11b487c2ab1a9a11078660c1618
Ryan McCue <ryanmccue@cubegames.net>
2011-03-22 19:20:59 +1000

    Bring Razor up-to-date with my copy.
    Apologies for the lack of commit history here. I lost the Git repository itself, which hadn't been pushed up, so we're missing a bunch of history.

--- index.php
+++ index.php
@@ -15,10 +15,15 @@ $authenticated = !!$user->identify();
 <head>
 	<title><?php echo get_option('sitename') ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php template_directory() ?>/style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php template_directory() ?>/resources/fancybox/fancybox.css" />
 	<script type="text/javascript" src="<?php echo get_option('baseurl') ?>inc/js/jquery.js"></script>
+	<script type="text/javascript" src="<?php template_directory() ?>/resources/raphael-min.js"></script>
+	<script type="text/javascript" src="<?php template_directory() ?>/resources/icons.js"></script>
+	
 	<script type="text/javascript" src="<?php echo get_option('baseurl') ?>inc/js/api.js"></script>
 	<script type="text/javascript" src="<?php template_directory() ?>/date.extensions.js"></script>
 	<script type="text/javascript" src="<?php template_directory() ?>/to_relative_time.jquery.js"></script>
+	<script type="text/javascript" src="<?php template_directory() ?>/resources/fancybox/fancybox.js"></script>
 	<script type="text/javascript" src="<?php template_directory() ?>/core.js"></script>
 	<?php
 	template_header();
@@ -29,19 +34,19 @@ $authenticated = !!$user->identify();
 
 		<h1><a href="#"><?php echo get_option('sitename') ?></a></h1>
 		<ul id="menu">
-			<li id="update"><a href="?method=update">Update</a></li>
+			<li id="update"><a href="?method=update" title="Update your feeds">Update</a></li>
 			<li id="updating">Now updating&hellip; <span class="progress"></span></li>
+			<li id="help"><a href="#help" title="Learn how to use Razor">Help</a></li>
 <?php
 if($authenticated) {
 ?>
-			<li><a href="<?php echo get_option('baseurl') ?>admin/settings.php">Settings</a></li>
-			<li><a href="#help" id="help">Help</a></li>
-			<li><a href="<?php echo get_option('baseurl') ?>admin/login.php?logout=logout&return=index.php">Logout</a></li>
+			<li id="settings"><a href="<?php echo get_option('baseurl') ?>admin/settings.php" title="Change your settings">Settings</a></li>
+			<li id="logout"><a href="<?php echo get_option('baseurl') ?>admin/login.php?logout=logout&return=index.php" title="Log out of Lilina">Log out</a></li>
 <?php
 }
 else {
 ?>
-			<li><a href="<?php echo get_option('baseurl') ?>admin/login.php?return=index.php">Login</a></li>
+			<li id="login"><a href="<?php echo get_option('baseurl') ?>admin/login.php?return=index.php">Log in</a></li>
 <?php
 }
 ?>
@@ -50,12 +55,35 @@ else {
 
 	<div id="sidebar">
 		<div class="item-list">
-			<!--<h2>Library</h2>
-			<ul id="library-list">
-				<li><a href="#everything">Everything</a></li>
-				<li><a href="#unread">Unread</a></li>
-			</ul>-->
+<?php
 
+$library = new LibraryView('library', 'Library');
+$everything = new LibraryView('everything', 'Everything');
+$everything->add_child(new LibraryView('most', 'Well, Most Things'));
+$library->add_child($everything);
+$library->add_child(new LibraryView('unread', 'Unread'));
+
+$menu = array(
+	'library' => $library
+);
+foreach ($menu as $id => $item) {
+?>
+	<h2><?php echo $item->get_title() ?></h2>
+	<ul id="<?php echo $item->id ?>">
+<?php
+	foreach ($item->get_children() as $child) {
+		echo print_library_item($child, $id);
+	}
+?>
+	</ul>
+<?php
+}
+?>
+			<h2>Other</h2>
+			<ul>
+				<li class="expandable"><a href="#"><span class="arrow">&#x25B6;</span><span class="text">Some Folder</span></a></li>
+				<li><a href="#"><img src="http://images.betanews.com/betanews2/icon_feed.png" />Some Folder</a></li>
+			</ul>
 			<h2>Feeds</h2>
 			<ul id="feeds-list">
 				<li><a href="#">Loading feeds...</a></li>
@@ -73,6 +101,13 @@ if($authenticated) {
 ?>
 			</ul>
 		</div>
+	</div>
+	
+	<div id="switcher" class="footer">
+		<ul>
+			<li><a href="#" id="switcher-sidebar">Sidebar</a></li>
+			<li><a href="#" id="switcher-items">Items</a></li>
+		</ul>
 	</div>
 
 	<div id="items-list">
