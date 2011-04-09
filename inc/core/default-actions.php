9afc5f9f0fef6ee1f8f4525dcb1769a9b4a1cc07
Ryan McCue <ryan@getlilina.org>
2011-04-09 16:05:08 +1000

    Hook the plugin updater check into admin_init

--- inc/core/default-actions.php
+++ inc/core/default-actions.php
@@ -5,6 +5,7 @@
  */
 
 add_action('admin_init', 'lilina_version_check');
+add_action('admin_init', array('Lilina_Updater_Plugins', 'admin_init'));
 
 add_action('template_header', 'template_synd_header');
 
