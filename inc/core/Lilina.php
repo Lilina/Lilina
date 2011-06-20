782d9b043a19d313669c426c4398f0b8bb04c1eb
Ryan McCue <me@ryanmccue.info>
2011-06-20 17:25:54 +1000

    Switch to HTML5 for nice_die

--- inc/core/Lilina.php
+++ inc/core/Lilina.php
@@ -128,25 +128,24 @@ class Lilina {
 		}
 		$guessurl = Lilina::guess_baseurl();
 	?>
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-			<title><?php echo $title; ?> &mdash; Lilina News Aggregator</title>
-			<link rel="stylesheet" type="text/css" href="<?php echo $guessurl ?>admin/resources/reset.css" />
-			<link rel="stylesheet" type="text/css" href="<?php echo $guessurl ?>install.css" />
-		</head>
-		<body<?php if($class !== false) echo ' class="' . $class . '"'; ?>>
-			<div id="content">
-				<h1 id="title"><?php echo $title; ?></h1>
-				<?php echo $message; ?>
-			</div>
-			<div id="footer">
-				<p>Powered by <a href="http://getlilina.org/">Lilina</a> <span class="version"><?php echo LILINA_CORE_VERSION; ?></span>. Read the <a href="http://codex.getlilina.org/">documentation</a> or get help on the <a href="http://getlilina.org/forums/">forums</a></p>
-			</div>
-		</body>
-	</html>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title><?php echo $title; ?> &mdash; Lilina News Aggregator</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo $guessurl ?>admin/resources/reset.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo $guessurl ?>install.css" />
+	</head>
+	<body<?php if($class !== false) echo ' class="' . $class . '"'; ?>>
+		<div id="content">
+			<h1 id="title"><?php echo $title; ?></h1>
+			<?php echo $message; ?>
+		</div>
+		<div id="footer">
+			<p>Powered by <a href="http://getlilina.org/">Lilina</a> <span class="version"><?php echo LILINA_CORE_VERSION; ?></span>. Read the <a href="http://codex.getlilina.org/">documentation</a> or get help on the <a href="http://getlilina.org/forums/">forums</a></p>
+		</div>
+	</body>
+</html>
 	<?php
 		die();
 	}
