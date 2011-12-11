b98bf31abc7385fb3ef98492a2915106d7a879f3
Ryan McCue <ryan@getlilina.org>
2011-12-11 20:24:28 +1000

    Add Items_Iterator::each()

--- inc/core/Lilina/Items/Iterator.php
+++ inc/core/Lilina/Items/Iterator.php
@@ -1,5 +1,9 @@
 <?php
 
 class Lilina_Items_Iterator extends ArrayIterator {
-	
+	public function each($callback) {
+		foreach ($this as $key => $val) {
+			call_user_func($callback, $key, $val);
+		}
+	}
 }
