db34a11ae85e167009b43c41bb2234d14f6c0e18
Ryan McCue <ryan@getlilina.org>
2011-12-11 15:56:55 +1000

    Add docs to Lilina_DB_Adapter_Base.

--- inc/core/Lilina/DB/Adapter/Base.php
+++ inc/core/Lilina/DB/Adapter/Base.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ * Base DB adapter
+ *
+ * @package Lilina
+ * @subpackage Database
+ */
 
+/**
+ * Base DB adapter
+ *
+ * @package Lilina
+ * @subpackage Database
+ */
 abstract class Lilina_Adapter_Base {
+	/**
+	 * Convert an object to an array
+	 *
+	 * Either uses $obj->_db_export($db_options) if it exists, or
+	 * get_object_vars() otherwise.
+	 *
+	 * @param object $obj
+	 * @return array Associative array of properties
+	 */
 	protected static function object_to_array(&$obj) {
 		if (is_callable(array($obj, '_db_export'), false, $callable)) {
 			return $callable($options);
