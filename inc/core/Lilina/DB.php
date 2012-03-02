ca8418b0cacd85c6e3a58db581528f6a5fd84a75
Ryan McCue <ryan@getlilina.org>
2012-03-03 00:13:20 +1000

    Change Feeds class to use Lilina_Feeds

--- inc/core/Lilina/DB.php
+++ inc/core/Lilina/DB.php
@@ -1,11 +1,18 @@
 <?php
 
 class Lilina_DB {
-	public static function get_adapter($args = null, $class = null) {
+	public static function &get_adapter($args = null, $class = null) {
+		static $adapter = null;
+
+		if (!empty($adapter)) {
+			return $adapter;
+		}
+
 		if (empty($class)) {
 			$class = get_option('dbdriver', 'Lilina_DB_Adapter_File');
 		}
 
-		return new $class($args);
+		$adapter = new $class($args);
+		return $adapter;
 	}
 }
